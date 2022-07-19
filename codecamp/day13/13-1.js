570d5e84e655ad83a458f3e9131611a321cd8338
Annie-Cho <ani100482@gmail.com>
2022-07-19 13:56:38 +0900

    13주차 01번 누락

--- codecamp/day13/13-1.js
+++ codecamp/day13/13-1.js
@@ -1,1 +1,17 @@
 /* 프로그래머스 : 없는 숫자 더하기 */
+function solution(numbers) {
+    var answer = 0;
+    let result = new Array(10)
+    
+    result.fill(0, 0)
+    numbers.forEach((ele) => {
+      result[ele]++
+    })
+  	
+  	result.forEach((ele, idx) => {
+      if(ele === 0) {
+        answer += idx
+      }
+    })
+    return answer;
+}
