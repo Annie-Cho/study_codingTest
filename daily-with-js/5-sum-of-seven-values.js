d8b971991f5f3ea8eb79e3535485bf3044804130
Annie-Cho <ani100482@gmail.com>
2025-01-14 11:12:13 +0900

    Feat: day2 오전 코테

---
+++ daily-with-js/5-sum-of-seven-values.js
@@ -0,0 +1,23 @@
+/**
+ * 최솟값 구하기
+ *
+ * 7개의 수가 주어지면 그 숫자 중 가장 작은 수를 출력하는 프로그램을 작성하세요.
+ */
+
+function solution(a, b, c, d, e, f, g){
+    // case 1) 빌트인 메소드 사용하기
+    return Math.min(a, b, c, d, e, f, g);
+
+    // case 2) for문 사용하기
+    // let answer = a;
+    // let numbers = [a, b, c, d, e, f, g];
+    //
+    // for(let i = 0; i<numbers.length; i++){
+    //     if(answer > numbers[i]){
+    //         answer = numbers[i];
+    //     }
+    // }
+    // return answer;
+}
+
+console.log(solution(7, 6, 5, 4, 3, 2, 1));
