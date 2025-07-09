60827c677045deb021d61b3647041875e22db6ee
Annie-Cho <ani100482@gmail.com>
2025-07-09 22:10:08 +0900

    added 10th solutions

---
+++ daily-with-js/10-find-char.js
@@ -0,0 +1,30 @@
+/**
+ * 한 개의 문자를 입력받고, 특정 문자열을 입력받은 후
+ * 문자열에 문자가 몇 개 존재하는지 알아내시오.
+ * ex) COMPUTERPROGRAMMING R
+ *
+ * 문자열의 길이는 100을 넘지 않는다.
+ */
+
+function findChar(newString, char) {
+  /* 정규식을 사용한 방법
+  const regex = new RegExp(`${char}`, "g");
+  const answer = newString.match(regex);
+  return answer.length;
+  */
+  //
+  /* for문 사용 
+  let count = 0;
+  for (let newChar of newString) {
+    if (newChar === char) count++;
+  }
+
+  return count;
+  */
+  //
+  /* 내장함수 사용 - char를 기준으로 문자열을 나누어 개수를 계산한다. */
+  const answer = newString.split(char);
+  return answer.length - 1;
+}
+
+console.log(findChar("COMPUTERPROGRAMMING", "R"));
