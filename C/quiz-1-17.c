3404e3e5a678925618217eb02acc6df17fcad2d6
Annie-Cho <ani100482@gmail.com>
2025-07-10 14:59:07 +0900

    chapter1 17th solution changes

--- C/quiz-1-17.c
+++ C/quiz-1-17.c
@@ -1,28 +1,28 @@
 /* n단의 피라미드를 출력하는 함수를 작성하시오 */
-// #include <stdio.h>
+#include <stdio.h>
 
-// void pyramid(int n){
-//     int i, j, k, l;
+void pyramid(int n){
+    int i, j, k, l;
 
-//     for(i=0; i<n; i++){
-//         for(j=0; j<(n*2-1)/2-i; j++){
-//             printf("_");
-//         };
-        
-//         for(k=0; k<(n*2-1); k+=2){
-//             printf("*");
-//         }
-//         printf("\n");
-//     }
-// }
+    for(i=1; i<=n; i++){
+        for(j=1; j<=n-i; j++){
+            printf(" ");
+        }
 
-// int main(void){
-//     int number;
+        for(j=1; j<=(i-1)*2+1; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+}
 
-//     printf("몇 단 피라미드입니까? ");
-//     scanf("%d", &number);
+int main(void){
+    int number;
 
-//     pyramid(number);
+    printf("몇 단 피라미드입니까? ");
+    scanf("%d", &number);
 
-//     return 0;
-// }
+    pyramid(number);
+
+    return 0;
+}
