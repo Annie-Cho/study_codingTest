fdd78101a52334fd49a4086f208f2d72730e0b39
Annie-Cho <ani100482@gmail.com>
2025-07-14 21:48:51 +0900

    added 22nd solution

---
+++ daily-with-js/22-get-rank.js
@@ -0,0 +1,39 @@
+/**
+ * 등수 구하기
+ *
+ * 각 학생의 점수에 기반하여 등수를 산정한다.
+ * 같은 점수가 입력되는 경우 동일 처리한다.
+ * 즉, 가장 높은 점수가 92점인데, 92점이 3명 존재한다면 1등이 3명이고 다음 학생은 4등이다.
+ *
+ * ex)
+ * 87 87 92 100 76
+ * => 3 3 2 1 5
+ */
+
+function getRank(scores) {
+  // answer 배열 1로 초기화
+  /* 내 정답
+  const answer = [];
+
+  for (let i = 0; i < scores.length; i++) {
+    answer[i] = 1;
+  }
+    */
+
+  /* Array.from() 사용하기 */
+  const answer = Array.from({ length: scores.length }, () => 1);
+
+  // 내 점수보다 큰 점수가 있는 경우 등수 + 1을 한다.
+  for (let i = 0; i < scores.length; i++) {
+    for (let j = 0; j < scores.length; j++) {
+      if (scores[i] < scores[j]) {
+        answer[i]++;
+      }
+    }
+  }
+
+  return answer;
+}
+
+console.log(getRank([87, 87, 92, 100, 76]));
+console.log(getRank([87, 89, 92, 100, 76]));
