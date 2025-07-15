ba6997107384f7b50297b95408643c8df1e5e208
Annie-Cho <ani100482@gmail.com>
2025-07-15 22:02:02 +0900

    second review added 23rd solution

---
+++ daily-with-js/review/23-2-cross-puzzle.js
@@ -0,0 +1,44 @@
+/**
+ * 격자판 최대합
+ *
+ * N*N의 격자판이 주어지면 각 행의 합, 각 열의 합, 두 대각선의 합 중 가장 큰 합을 출력한다.
+ */
+
+function findMaxCrossPuzzle(arr) {
+  const length = arr.length;
+  let max = Number.MIN_SAFE_INTEGER;
+  let sum1 = 0;
+  let sum2 = 0;
+
+  for (let i = 0; i < length; i++) {
+    sum1 = 0;
+    sum2 = 0;
+    for (let j = 0; j < length; j++) {
+      // 각 행의 합
+      sum1 += arr[i][j];
+      // 각 열의 합
+      sum2 += arr[j][i];
+    }
+    max = Math.max(max, sum1, sum2);
+  }
+
+  sum1 = 0;
+  sum2 = 0;
+  for (let i = 0; i < length; i++) {
+    // 대각선(왼->오)의 합
+    sum1 = arr[i][i];
+    // 대각선(오->왼)의 합
+    sum2 = arr[length - 1 - i][i];
+  }
+  max = Math.max(max, sum1, sum2);
+  return max;
+}
+
+const arr = [
+  [10, 13, 10, 12, 15],
+  [12, 30, 30, 23, 11],
+  [11, 25, 50, 53, 15],
+  [19, 27, 29, 37, 27],
+  [19, 13, 30, 13, 19],
+];
+console.log(findMaxCrossPuzzle(arr));
