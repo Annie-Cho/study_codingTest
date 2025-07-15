33f6326bfc9233bac9286fe1490b8e75c3734793
Annie-Cho <ani100482@gmail.com>
2025-07-15 22:59:00 +0900

    added 27th solution

---
+++ daily-with-js/27-extract-number.js
@@ -0,0 +1,25 @@
+/**
+ * 숫자만 추출
+ *
+ * 문자와 숫자가 섞여있는 문자열이 주어지면 그 중 숫자만 추출하여 자연수로 만들어라.
+ * ex) tge0a1h205er -> 1205
+ */
+
+function extractNumber(newString) {
+  /* 정규식 활용하기 
+  const numberString = newString.match(/[0-9]/g);
+  return Number(numberString.join(""));
+  */
+
+  /* 내장함수 활용하기 */
+  let numberString = "";
+  for (let word of newString) {
+    if (!isNaN(word)) {
+      numberString += word;
+    }
+  }
+
+  return Number(numberString); // or parseInt(numberString)
+}
+
+console.log(extractNumber("tge0a1h205er"));
