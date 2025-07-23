68c1985801de0108d2a469b1604387cfc3ad563a
Annie-Cho <ani100482@gmail.com>
2025-07-23 23:30:08 +0900

    added 35th solution

---
+++ daily-with-js/35-combine-two-array.js
@@ -0,0 +1,46 @@
+/**
+ * 두 배열 합치기
+ *
+ * 오름차순으로 정렬된 두 배열을 오름차순으로 합쳐 출력하시오.
+ *
+ * ex)
+ * 첫 번째 배열 -> 1, 3, 5
+ * 두 번째 배열 -> 2, 3, 6, 7, 9
+ * => 1, 2, 3, 3, 5, 6, 7, 9
+ */
+
+function combineTwoArray(first = [], second = []) {
+  /** 아래와 같이 sort 사용 시
+   * 시간복잡도 : O(nxlogN) = O(NlogN) : 0.021ms
+   * 호호호 이런 경우 투포인터 사용하기 호호호
+   * 
+  const answer = [...first, ...second].sort((a, b) => a - b);
+  return answer;
+  */
+
+  /** O(n+m) : 0.004s*/
+  let p1 = 0;
+  let p2 = 0;
+  const answer = [];
+  while (p1 < first.length && p2 < second.length) {
+    if (first[p1] <= second[p2]) {
+      answer.push(first[p1]);
+      p1++;
+    } else {
+      answer.push(second[p2]);
+      p2++;
+    }
+  }
+
+  if (p1 === first.length) {
+    answer.push(...second.slice(p2));
+  } else if (p2 === second.length) {
+    answer.push(...first.slice(p1));
+  }
+
+  return answer;
+}
+
+const first = [1, 3, 5];
+const second = [2, 3, 6, 7, 9];
+console.log(combineTwoArray(first, second));
