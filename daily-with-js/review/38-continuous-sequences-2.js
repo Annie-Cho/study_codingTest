421ee9977f1b6f3f45e7771b231f1a1da6797caf
Annie-Cho <ani100482@gmail.com>
2025-07-25 23:38:44 +0900

    review added 38th solution

---
+++ daily-with-js/review/38-continuous-sequences-2.js
@@ -0,0 +1,35 @@
+/**
+ * 연속 부분수열 2
+ *
+ * N개의 수로 이루어진 수열이 있다.
+ * 연속 부분수열의 합이 M 이하가 되는 경우가 몇 번있는지 구하시오.
+ *
+ * ex) N=5, M=5
+ * 1, 3, 1, 2, 3
+ * => 10
+ * => {1}, {3}, {1}, {2}, {3},
+ * {1, 3}, {3, 1}, {1, 2}, {2, 3}, {1, 3, 1}
+ */
+
+function findContinuousSequences(sequence = [], target = 0) {
+  let p1 = 0;
+  let sum = 0;
+  let answer = 0;
+
+  for (let p2 = 0; p2 < sequence.length; p2++) {
+    sum += sequence[p2];
+
+    while (sum > target) {
+      sum -= sequence[p1];
+      p1++;
+    }
+    console.log(sequence.slice(p1, p2 + 1));
+    answer += p2 - p1 + 1;
+  }
+
+  return answer;
+}
+
+const sequence = [1, 3, 1, 2, 3];
+const target = 5;
+console.log(findContinuousSequences(sequence, target));
