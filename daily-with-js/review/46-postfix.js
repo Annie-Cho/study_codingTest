bd2c83b35f45b9052952e82dac8f4c206d947270
Annie-Cho <ani100482@gmail.com>
2025-08-01 22:08:09 +0900

    review added 46th solution

---
+++ daily-with-js/review/46-postfix.js
@@ -0,0 +1,37 @@
+/**
+ * 후위식 연산(postfix)
+ *
+ * 후위 연산식의 결과를 출력하시오.
+ * 만약 3 * (5 + 2) - 9 를 후위 연산식으로 표현하면 352+*9-이다.
+ *
+ * ex) 352+*9-
+ * => 12
+ */
+
+function postfix(str = "") {
+  const stack = [];
+
+  for (let letter of str) {
+    if (isNaN(letter)) {
+      const second = stack.pop();
+      const first = stack.pop();
+
+      if (letter === "+") {
+        stack.push(first + second);
+      } else if (letter === "*") {
+        stack.push(first * second);
+      } else if (letter === "-") {
+        stack.push(first - second);
+      } else if (letter === "/") {
+        stack.push(first / second);
+      }
+    } else {
+      stack.push(Number(letter));
+    }
+  }
+
+  return stack.pop();
+}
+
+const str = "352+*9-";
+console.log(postfix(str));
