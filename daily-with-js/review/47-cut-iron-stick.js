f0329a71b76e39fb1b47e24b59ac5f0198e7de31
Annie-Cho <ani100482@gmail.com>
2025-08-04 22:16:12 +0900

    review added 47th solution

---
+++ daily-with-js/review/47-cut-iron-stick.js
@@ -0,0 +1,38 @@
+/**
+ * 쇠막대기
+ *
+ * 여러 개의 쇠막대기를 레이저로 절단한다.
+ * 쇠막대기를 아래에서 위로 겹쳐놓고, 레이저를 위에서 수직으로 발사하여 쇠막대기들을 자른다.
+ *
+ * 쇠막대기는 자신보다 긴 쇠막대기 위에만 놓일 수 있다.
+ * 쇠막대기를 자르는 레이저는 적어도 하나 존재한다.
+ * 레이저는 어떤 쇠막대기의 양 끝점과도 겹치지 않는다.
+ *
+ * 잘려진 쇠막대기 조각의 총 개수를 구하시오.
+ * ex)()(((()())(())()))(())
+ * => 17
+ */
+
+function cutIronSticks(sticks) {
+  const stack = [];
+  let answer = 0;
+
+  for (let i = 0; i < sticks.length; i++) {
+    if (sticks[i] === "(") {
+      stack.push(sticks[i]);
+    } else if (sticks[i] === ")") {
+      if (sticks[i - 1] === "(") {
+        stack.pop();
+        answer += stack.length;
+      } else {
+        stack.pop();
+        answer += 1;
+      }
+    }
+  }
+
+  return answer;
+}
+
+const sticks = "()(((()())(())()))(())";
+console.log(cutIronSticks(sticks));
