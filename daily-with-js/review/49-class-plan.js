7b1e6a6d7f961edf54c4899ed0f2b2e799102c2e
Annie-Cho <ani100482@gmail.com>
2025-08-05 22:27:48 +0900

    review added 49th solution

---
+++ daily-with-js/review/49-class-plan.js
@@ -0,0 +1,54 @@
+/**
+ * 교육과정 설계
+ *
+ * 수업 중에는 필수 과목이 있다. 필수 과목은 반드시 이수해야하며, 그 순서가 정해져있다.
+ * 총 과목이 ABCDEFG인 경우 필수과목이 CBA로 주어지면 C -> B -> A 순서대로 수업계획을 짜야한다.
+ *
+ * CBDAGE -> 올바른 설계
+ * CGEADB -> 잘못된 설계
+ *
+ * 수업 설계가 잘 되어있다면 YES, 잘못되었다면 NO를 출력하시오.
+ *
+ * ex) CBA
+ * CBDAGE
+ * => YES
+ */
+
+/**
+ * 내가 푼 풀이
+ */
+function classPlan(essential = "", plan = "") {
+  const queue = essential.split("");
+
+  for (let p of plan) {
+    const index = queue.indexOf(p);
+    if (index === 0) {
+      queue.shift();
+    } else if (index > 0) {
+      return "NO";
+    }
+  }
+
+  return queue.length === 0 ? "YES" : "NO";
+}
+
+/**
+ * 강의 풀이
+function classPlan(essential = "", plan = "") {
+  const queue = essential.split("");
+
+  for (let p of plan) {
+    if (queue.includes(p)) {
+      if (queue.shift() !== p) {
+        return "NO";
+      }
+    }
+  }
+
+  return queue.length === 0 ? "YES" : "NO";
+}
+   */
+
+console.log(classPlan("CBA", "CBDAGE"));
+console.log(classPlan("CBA", "CADBGE"));
+console.log(classPlan("CBA", "DCBAGE"));
