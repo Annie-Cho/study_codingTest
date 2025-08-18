ac9f087f8bee96b36e58f4b9ad880a233d95ee4f
Annie-Cho <ani100482@gmail.com>
2025-08-18 22:48:16 +0900

    add 59th solution

---
+++ daily-with-js/review/59-binary-search.js
@@ -0,0 +1,33 @@
+/**
+ * 이분검색
+ *
+ * 임의의 N개의 수를 오름차순으로 정렬한 다음 하나의 숫자 M이 주어지면
+ * 이분검색으로 M이 몇 번째에 위치해있는지 구하시오.
+ *
+ * ex) [23, 87, 65, 12, 57, 32, 99, 81], 32
+ * => 3
+ */
+
+function binarySearch(numbers, target) {
+  numbers.sort((a, b) => a - b);
+
+  let left = 0;
+  let right = numbers.length - 1;
+  let answer = 0;
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+
+    if (numbers[mid] === target) {
+      answer = mid + 1;
+      break;
+    } else if (numbers[mid] > target) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+
+  return answer;
+}
+
+console.log(binarySearch([23, 87, 65, 12, 57, 32, 99, 81], 32));
