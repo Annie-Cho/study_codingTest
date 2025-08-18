4067f1aac79dbc9ab80e6cdfa17288789aa8c60e
Annie-Cho <ani100482@gmail.com>
2025-08-18 21:49:31 +0900

    fifth review added 54th solution

---
+++ daily-with-js/review/54-5-least-recently-used.js
@@ -0,0 +1,55 @@
+/**
+ * Least Recently Used(카카오 캐시 문제 변형)
+ *
+ * ex) 작업개수 : 5, 작업 : 1 2 3 2 6 2 3 5 7
+ * => 7 5 3 2 6
+ */
+
+/* for문 사용하기 
+function leastRecentlyUsed(cacheSize, jobs) {
+  const cache = Array.from({ length: cacheSize }, () => 0);
+
+  for (let job of jobs) {
+    const curIndex = cache.indexOf(job);
+
+    if (curIndex >= 0) {
+      for (let i = curIndex - 1; i >= 0; i--) {
+        cache[i + 1] = cache[i];
+      }
+    } else {
+      for (let i = cache.length - 1; i >= 0; i--) {
+        cache[i + 1] = cache[i];
+      }
+    }
+    cache[0] = job;
+
+    if (cache.length > cacheSize) {
+      cache.pop();
+    }
+  }
+
+  return cache;
+}
+  */
+
+/* 내장함수 사용하기 */
+function leastRecentlyUsed(cacheSize, jobs) {
+  const cache = Array.from({ length: cacheSize }, () => 0);
+
+  for (let job of jobs) {
+    const curIndex = cache.indexOf(job);
+
+    if (curIndex >= 0) {
+      cache.splice(curIndex, 1);
+    }
+    cache.unshift(job);
+
+    if (cache.length > 5) {
+      cache.pop();
+    }
+  }
+
+  return cache;
+}
+
+console.log(leastRecentlyUsed(5, [1, 2, 3, 2, 6, 2, 3, 5, 7]));
