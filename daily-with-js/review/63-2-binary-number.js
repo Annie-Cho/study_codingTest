dfb68777f2375dd756b1fe33d286c33a5196e4cb
Annie-Cho <ani100482@gmail.com>
2025-08-26 22:34:52 +0900

    second review added 63th solution

---
+++ daily-with-js/review/63-2-binary-number.js
@@ -0,0 +1,25 @@
+/**
+ * 이진수 출력
+ *
+ * 10진수 N이 입력되면 2진수로 변환하여 출력하시오.
+ * 단, 재귀함수를 사용하시오.
+ *
+ * ex) 11
+ * => 1011
+ */
+
+function binaryNumber(num) {
+  let answer = "";
+
+  function DFS(num) {
+    if (num === 0) return;
+
+    DFS(Math.floor(num / 2));
+    answer += num % 2;
+  }
+
+  DFS(num);
+  return answer;
+}
+
+console.log(binaryNumber(11));
