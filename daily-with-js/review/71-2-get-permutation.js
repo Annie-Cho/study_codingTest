e72f0d6a2700b82723f4823943db5e762df98009
Annie-Cho <ani100482@gmail.com>
2025-09-11 23:06:20 +0900

    second review added 71th solution

---
+++ daily-with-js/review/71-2-get-permutation.js
@@ -0,0 +1,42 @@
+/**
+ * 순열 구하기
+ *
+ * 10 이하의 N개의 자연수가 주어지면
+ * 이 중 M개를 뽑아 일렬로 나열하는 방법을 모두 출력하시오.
+ *
+ * ex) 2(M)
+ * [3, 6, 9](자연수)
+ * => 3 6
+ * 3 9
+ * 6 3
+ * 6 9
+ * 9 3
+ * 9 6
+ * 6(총 경우의 수)
+ */
+
+function permutation(pickCount, numbers) {
+  const answer = [];
+  const pickNum = Array.from({ length: pickCount }, () => 0);
+  const check = Array.from({ length: numbers.length }, () => 0);
+
+  function DFS(n) {
+    if (n === pickCount) {
+      answer.push([...pickNum]);
+    } else {
+      for (let i = 0; i < numbers.length; i++) {
+        if (check[i] === 0) {
+          check[i] = 1;
+          pickNum[n] = numbers[i];
+          DFS(n + 1);
+          check[i] = 0;
+        }
+      }
+    }
+  }
+
+  DFS(0);
+  return [answer, answer.length];
+}
+
+console.log(permutation(2, [3, 6, 9]));
